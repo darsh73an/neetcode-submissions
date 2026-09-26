e4f109872361cf94166a7e36508ca37597392265
Darshan R <darshankok7@gmail.com>
2026-09-26 18:56:53 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i : s){
+            if(i == '(' || i == '{' || i == '['){
+                st.push(i);
+            }else{
+                if(st.empty()){
+                    return false; // bcoz there is closing but st is empty so
+                }
+
+                char ch = st.top();
+
+                if(ch != '(' && i == ')') return false;  // top is open and curr is not closing
+                if(ch != '[' && i == ']') return false;
+                if(ch != '{' && i == '}') return false;
+
+                st.pop();
+            }
+        }
+        return st.empty(); // must bcoz if all are opening
+    }
+};
+
+// tc 0(n)
+// sc 0(n)
